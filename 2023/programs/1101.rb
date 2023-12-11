26c051d4d579575a0bce2ffcabaf5a04bb9c7429
John Holdun <john@johnholdun.com>
2023-12-10 21:27:19 -0800

    2023-12-11

---
+++ 2023/programs/1101.rb
@@ -0,0 +1,35 @@
+class Response
+  def call(lines)
+    galaxies = []
+
+    lines.each_with_index do |line, y|
+      line.split('').each_with_index do |char, x|
+        galaxies.push([x, y]) if char == '#'
+      end
+    end
+
+    rows = lines.size
+    columns = lines.first.size
+
+    empty_rows = rows.times.select { |y| galaxies.none? { |_, y2| y2 == y } }
+    empty_columns = columns.times.select { |x| galaxies.none? { |x2, _| x2 == x } }
+
+    galaxies.map! do |x, y|
+      [
+        x + empty_columns.count { |x2| x2 < x },
+        y + empty_rows.count { |y2| y2 < y }
+      ]
+    end
+
+    sum = 0
+
+    galaxies.each_with_index do |(x1, y1), i1|
+      galaxies.each_with_index do |(x2, y2), i2|
+        next if i2 <= i1
+        sum += (x1 - x2).abs + (y1 - y2).abs
+      end
+    end
+
+    sum
+  end
+end
