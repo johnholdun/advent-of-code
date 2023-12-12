adaf5457efb9064bb4740efab5331e00fd703eb5
John Holdun <john@johnholdun.com>
2023-12-11 20:52:50 -0800

    2021-12-12 part 2 (amended)

--- 2021/programs/2102.rb
+++ 2021/programs/2102.rb
@@ -1,109 +1,132 @@
 class Response
-  SPLITS =
-    %w(111 112 113 121 122 123 131 132 133 211 212 213 221 222 223 231 232 233 311 312 313 321 322 323 331 332 333)
-      .map { |c| c.split('').map(&:to_i) }
-  FREQUENCIES =
-    SPLITS
-      .map { |s| s.inject(:+) }
-      .group_by(&:itself)
-      .map { |s, cs| [s, cs.size] }
-      .to_h
+  USE_BROKEN_IDEA = false
+
+  ROLLS =
+    [1, 2, 3]
+      .product([1, 2, 3])
+      .product([1, 2, 3])
+      .map { |r| r.flatten.sum }
+      .sort
+      .freeze
 
   def call(input)
-    positions = input.map { |l| l.split(' ').last.to_i }
-    win_frequencies = positions.map { |p| winning_rolls(p) }
-    wins = [0, 0]
-    every_possible_turn_count = win_frequencies.flat_map(&:keys).uniq.sort
-    wins_by_turn_count = every_possible_turn_count.map { |l| [l, win_frequencies.map { |h| h[l].to_i }] }.to_h
+    if USE_BROKEN_IDEA
+      # The idea here, which doesn't seem to work yet but I don't know why:
+      #
+      # 1. For each starting position, find all the possible wins by number of
+      #    turns. This should (I think?) represent every possible game played by
+      #    this player, because even if they other player does nothing, this
+      #    player is guaranteed to win by turn 21.
+      # 2. Consider that every one of those games is played against one of the
+      #    games recorded for the other player. Compare these batches of games
+      #    and mark a win based on who took fewer turns.
+      # 3. Add up all the wins by player!
 
-    wins_by_turn_count.each do |turn_count, win_frequencies|
-      wins_at_this_length = win_frequencies.sum
-      plays_at_this_length = 27 ** turn_count
-      games_at_this_length = wins_at_this_length * (plays_at_this_length - wins_at_this_length)
-      wins[0] += games_at_this_length * 0.5
-      wins[1] += games_at_this_length * 0.5
-      # win_frequencies.each_with_index do |frequency, index|
-      #   other_index = index.zero? ? 1 : 0
-      #   max_possible_losing_plays = 27 ** turn_count
-      #   winning_plays =
-      #     wins_by_turn_count.keys.select do |key|
-      #       (index.zero? ? key < turn_count : key <= turn_count)
-      #     end.map { |k| wins_by_turn_count[k][other_index] }.sum
-      #   losing_plays = max_possible_losing_plays - wins[other_index]
-      #   wins[index] += frequency * losing_plays
-      # end
+      positions = input.map { |i| i.scan(/\d+$/).last.to_i }
+
+      # A weird thing is that `wins_by_turns` seems to return the same result
+      # regardless of the input. I'm not smart enough to know if that means
+      # there's a bug in my code, or the score outcomes are actually independent
+      # of the starting position!
+      a, b = positions.map { |p| wins_by_turns(p) }
+
+      a_wins =
+        a.map do |turns, wins|
+          wins * b.select { |k, v| k >= turns }.values.sum
+        end
+        .sum
+
+      b_wins =
+        b.map do |turns, wins|
+          wins * a.select { |k, v| k > turns }.values.sum
+        end
+        .sum
+
+      [a_wins, b_wins].max
+    else
+      @states = {}
+      position1, position2 = input.map { |i| i.scan(/\d+$/).last.to_i }
+      # Shout out to Jonathan Paulson for sharing and explaining his solution:
+      # https://github.com/jonathanpaulson/AdventOfCode/blob/master/2021/21.py
+      # https://www.youtube.com/watch?v=a6ZdJEntKkk
+      #
+      # I'm going to re-explain this so I feel good about getting credit for my
+      # version of the solution.
+      #
+      # Even though there are 27 possible outcomes after each player's turn, the
+      # board is small and cyclical so there are only so many possible states that
+      # any game can be in. The fact that no score will ever exceed 21 (or be
+      # slightly over 21) also keeps the number of states low. We can store every
+      # possible state as a combination of each player's current position and
+      # current score, and therefore save a ton of time on calculation.
+      #
+      # To be honest this memoization seems to me to not be saving that much
+      # computation, but I guess it really adds up! Computers love to look up
+      # numbers they've already seen way more than they love adding them together
+      # (but let's be clear, they love ALL of it).
+      jonathan(position1, position2).max
     end
-
-    return wins.sum + 0
-
-    positions.each_with_index.map do |position, index|
-      other_position = positions[index.zero? ? 1 : 0]
-      winning_rolls(position).each do |turn_count, frequency|
-        # if the other player would win in fewer turns, this game would never
-        # actually get to this turn count, so we disregard it. in a tie, this is
-        # a victory for player 1 (index 0).
-        defeating_frequency =
-          winning_rolls(other_position)
-            .keys
-            .select { |c| index.zero? ? c <= turn_count : c <= turn_count }
-            .map { |k| winning_rolls(other_position)[k] }
-            .inject(:+)
-            .to_i
-
-        # any other plays by the opponent will lose
-        games_at_least_this_long = 27 ** turn_count
-        player_turns_that_end_here = frequency
-        possible_moves_by_other_player = games_at_least_this_long
-        games_that_end_before_here
-        wins[index] += frequency * (27 ** turn_count - defeating_frequency)
-      end
-    end
-
-    wins.inspect
-
-    # every player is gauranteed to hit 21 after no more than 21 turns, because
-    # they get at least 1 point per turn
-    # each player's game is independent of the other, so we're just figuring out how long it takes each player to win, and how many of the other player's games last longer than that
-    # take all winning rolls for each player's starting position and
-    # expand them based on the frequencies of each total roll (i.e. 1/27
-    # universes has a roll on a turn of 1+1+1)
-    # then somehow figure out when those
   end
 
-  private
+  def jonathan(position1, position2, score1 = 0, score2 = 0)
+    return [1, 0] if score1 >= 21
+    return [0, 1] if score2 >= 21
 
-  def winning_paths
-    return @winning_paths if defined?(@winning_paths)
-    @winning_paths = []
-    paths = (1..10).map { |n| [[0, n]] }
-    new_paths = []
-    loop do
-      paths.each do |path|
-        if path.map(&:last).inject(:+) - path.first.last >= 21
-          @winning_paths.push(path)
-        else
-          (3..9).each do |move|
-            new_paths.push(path + [[move, (path.last.last - 1 + move) % 10 + 1]])
+    key = [position1, position2, score1, score2]
+    return @states[key] if @states.key?(key)
+
+    wins = [0, 0]
+
+    ROLLS.each do |move|
+      new_position1 = (position1 + move - 1) % 10 + 1
+
+      # This is definitely not something I would have picked up without
+      # Jonathan's help, but look: we actually flip players 1 and 2 here to
+      # calculate the other player's score, because we can memoize them in
+      # either order as long as the position and score stay related. It feels
+      # like there's a further optimization to be made here because each
+      # player's game is actually independent of the other's, and all we need to
+      # know is when a given position and a given number of rolls reaches the
+      # winning score, then compare the number of turns to the number of turns
+      # for the same rolls starting from the other position. But I don't really
+      # understand what I'm saying so I'm glad to have had Jonathan's help.
+      win_2, win_1 =
+        jonathan \
+          position2,
+          new_position1,
+          score2,
+          score1 + new_position1
+
+      wins[0] += win_1
+      wins[1] += win_2
+    end
+
+    @states[key] = wins
+  end
+
+  def wins_by_turns(position)
+    wins = Hash.new(0)
+    games = [[position, 0]]
+
+    # you get at least 1 point per turn so there will never be more than 21
+    # turns to get to 21
+    (1..21).each do |turns|
+      old_games = games
+      games = []
+      old_games.each do |position, score|
+        ROLLS.each do |roll|
+          new_position = (position + roll - 1) % 10 + 1
+          new_score = score + roll
+
+          if new_score >= 21
+            wins[turns] += 1
+          else
+            games.push([new_position, new_score])
           end
         end
       end
-      break if new_paths.size.zero?
-      paths = new_paths
-      new_paths = []
     end
-    @winning_paths
-  end
 
-  def winning_rolls(starting_position)
-    @winning_rolls ||= {}
-    @winning_rolls[starting_position] ||=
-      winning_paths
-        .select { |path| path.first.last == starting_position }
-        .map { |path| path.map(&:first)[1..-1] }
-        .group_by(&:size)
-        .map do |turn_count, all_moves|
-          [turn_count, all_moves.map { |moves| moves.map { |m| FREQUENCIES[m] }.inject(:*) }.inject(:+)]
-        end
-        .to_h
+    wins
   end
 end
